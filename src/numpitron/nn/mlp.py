579d237e642216cea65150eb0bb1a47d45dfb071
laurens <laurensweitkamp@thirona.eu>
2024-03-12 13:41:13 -0700

    add general linear and MLP layers

---
+++ src/numpitron/nn/mlp.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from numpitron import nn
+from numpitron.nn.core import Sequential
+
+
+class MLP(Sequential):
+    """Simple Multi-Layered Perceptron with two layers."""
+
+    def __init__(self, d_model: int, d_hidden: int, name="MLP", dtype=np.float32):
+        super().__init__(name=name, dtype=dtype)
+        self.d_model = d_model
+        self.d_hidden = d_hidden
+
+        self.layers.extend([
+            nn.Linear(d_model, d_hidden, f"{name}_MLP_Linear1", dtype),
+            nn.ReLU(),
+            nn.Linear(d_hidden, d_model, f"{name}_MLP_Linear2", dtype),
+        ])
