fe1868c1a23fba57b7f106509a9018cc6ce1a533
laurens <laurensweitkamp@thirona.eu>
2024-03-16 15:22:15 -0700

    add nn imports

---
+++ src/numpitron/nn/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+from numpitron.nn.linear import Linear
+from numpitron.nn.mlp import MLP
+from numpitron.nn.activation import ReLU
+from numpitron.nn.embedding import InputEmbedding, OutputEmbedding, PositionalEmbedding
