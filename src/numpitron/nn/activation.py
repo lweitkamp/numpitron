c9ca880b40779b6a0c9cdeec17042a78dc7c5ee2
laurens <laurensweitkamp@thirona.eu>
2024-03-18 14:12:05 -0700

    set context as first input and output arg

--- src/numpitron/nn/activation.py
+++ src/numpitron/nn/activation.py
@@ -7,9 +7,11 @@ class ReLU(Layer):
     def __init__(self, name: str = "ReLU"):
         super().__init__(name=name, dtype=None)
 
-    def forward(self, params, inputs: np.ndarray) -> np.ndarray:
-        return np.maximum(0, inputs), {"inputs": np.copy(inputs)}
+    def forward(
+        self, params: dict[str, np.ndarray], inputs: np.ndarray
+    ) -> tuple[dict, np.ndarray]:
+        return {"inputs": np.copy(inputs)}, np.maximum(0, inputs)
 
-    def backward(self, d_out: np.ndarray, ctx: dict) -> tuple[np.ndarray, dict]:
+    def backward(self, ctx: dict, d_out: np.ndarray) -> tuple[dict, np.ndarray]:
         d_out = (ctx["inputs"] > 0) * d_out
-        return d_out, {}
+        return {}, d_out
