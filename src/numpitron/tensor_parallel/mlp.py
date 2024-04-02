551ee428019156f78b72b48677a2b02c22561472
laurensweitkamp <laurensweitkamp@thirona.eu>
2024-04-02 20:06:25 +0200

    add megatron-lm MLP

---
+++ src/numpitron/tensor_parallel/mlp.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+from numpitron import nn
+from numpitron.nn.core import Sequential, tree_map
+
+from numpitron import distributed as npdist
+
+
+class TensorParallelMLP(Sequential):
+    """Simple Multi-Layered Perceptron with two layers."""
+
+    def __init__(
+        self,
+        d_model: int,
+        d_hidden: int,
+        communicator: npdist.ParallelCommunicator,
+        name="MLP",
+        dtype=np.float32,
+    ):
+        super().__init__(name=name, dtype=dtype)
+        self.d_model = d_model
+        self.d_hidden = d_hidden
+        self.comm = communicator
+
+        self.layers.append(
+            nn.Linear(d_model, d_hidden // self.comm.tp_size, "ColumnLinear", dtype)
+        )
+        self.layers.append(nn.ReLU())
+        self.layers.append(
+            nn.Linear(d_hidden // self.comm.tp_size, d_model, "RowLinear", dtype),
+        )
+
+    def forward(
+        self, params: dict[str, np.ndarray], inputs: np.ndarray
+    ) -> tuple[dict, np.ndarray]:
+        ctxs, inputs = super().forward(params, inputs)
+        tree_map(npdist.all_reduce, inputs, comm=self.comm.tp_comm)
+        return ctxs, inputs
+
+    def backward(
+        self,
+        ctx: dict[str, dict],
+        d_out: np.ndarray,
+    ) -> tuple[dict[str, dict], np.ndarray]:
+        gradients, d_out = super().backward(ctx, d_out)
+        tree_map(npdist.all_reduce, gradients, comm=self.comm.tp_comm)
+        return gradients, d_out
---
+++ tests/test_tensor_parallel.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pytest
+from numpy.random import Generator
+
+import numpitron.distributed as npdist
+from numpitron import nn
+from numpitron.tensor_parallel import TensorParallelMLP
+
+world_comm = npdist.WorldCommunicator()
+RANK = world_comm.world_rank
+WORLD_SIZE = world_comm.world_size
+
+comm = npdist.ParallelCommunicator(tp_size=WORLD_SIZE)
+
+
+TEST_SHAPES = [
+    (1, 1, 8),
+    (3, 1, 8),
+    (1, 3, 8),
+    (3, 3, 8),
+]
+
+
+@pytest.fixture
+def rng() -> Generator:
+    return np.random.default_rng(42)
+
+
+@pytest.mark.parametrize("batch_size,seq_len,d_model", TEST_SHAPES)
+def test_mlp(
+    batch_size: int,
+    seq_len: int,
+    d_model: int,
+    rng: Generator,
+):
+    inputs = rng.random((batch_size, seq_len, d_model)).astype(np.float32)
+
+    mlp = nn.MLP(d_model, d_model * 4)
+    mlp_tp = TensorParallelMLP(d_model, d_model * 4, communicator=comm)
+    params = mlp.init_params(rng=rng)
+    params_tp = mlp_tp.init_params(rng=rng)
+
+    npdist.scatter(
+        params["Linear1"]["weight"],
+        params_tp["ColumnLinear"]["weight"],
+        axis=1,
+        comm=comm.tp_comm,
+    )
+    npdist.scatter(
+        params["Linear1"]["bias"],
+        params_tp["ColumnLinear"]["bias"],
+        axis=0,
+        comm=comm.tp_comm,
+    )
+    npdist.scatter(
+        params["Linear2"]["weight"],
+        params_tp["RowLinear"]["weight"],
+        axis=0,
+        comm=comm.tp_comm,
+    )
+
+    ctx, out = mlp(params, inputs)
+    ctx_tp, out_tp = mlp_tp(params_tp, inputs)
+
+    grads, d_out = mlp.backward(ctx, np.ones_like(out))
+    grads_tp, d_out_tp = mlp_tp.backward(ctx_tp, np.ones_like(out_tp))
+
+    np.testing.assert_allclose(d_out, d_out_tp, rtol=1e-5, atol=1e-5)
+    np.testing.assert_allclose(out, out_tp, atol=1e-5)
+
+    # np.testing.assert_allclose(
+    #     grads["Linear2"]["weight"].T,
+    #     mlp_torch[2].weight.grad,
+    #     rtol=1e-5,
+    #     atol=1e-5,
+    # )
+    # np.testing.assert_allclose(
+    #     grads["Linear2"]["bias"],
+    #     mlp_torch[2].bias.grad,
+    #     rtol=1e-5,
+    #     atol=1e-5,
+    # )
+    # np.testing.assert_allclose(
+    #     grads["Linear1"]["weight"].T,
+    #     mlp_torch[0].weight.grad,
+    #     rtol=1e-5,
+    #     atol=1e-5,
+    # )
+    # np.testing.assert_allclose(
+    #     grads["Linear1"]["bias"],
+    #     mlp_torch[0].bias.grad,
+    #     rtol=1e-5,
+    #     atol=1e-5,
+    # )
