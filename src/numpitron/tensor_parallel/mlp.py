dd33c10ce16cb8e5111762c1f076cd693c251907
laurensweitkamp <laurensweitkamp@thirona.eu>
2024-04-03 16:34:38 +0200

    better

--- src/numpitron/tensor_parallel/mlp.py
+++ src/numpitron/tensor_parallel/mlp.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 from numpitron import nn
-from numpitron.nn.core import Sequential, tree_map
+from numpitron.nn.core import tree_map
 
 from numpitron import distributed as npdist
 
 
-class TensorParallelMLP(Sequential):
+class TensorParallelMLP(nn.MLP):
     """Simple Multi-Layered Perceptron with two layers."""
 
     def __init__(
@@ -17,25 +17,20 @@ class TensorParallelMLP(Sequential):
         name="TensorParallelMLP",
         dtype=np.float32,
     ):
-        super().__init__(name=name, dtype=dtype)
-        self.d_model = d_model
-        self.d_hidden = d_hidden
+        super().__init__(
+            d_model=d_model,
+            d_hidden=d_hidden // communicator.tp_size,
+            name=name,
+            dtype=dtype,
+        )
         self.comm = communicator
-
-        self.layers.append(
-            nn.Linear(d_model, d_hidden // self.comm.tp_size, "ColumnLinear", dtype)
-        )
-        self.layers.append(nn.ReLU())
-        self.layers.append(
-            nn.Linear(d_hidden // self.comm.tp_size, d_model, "RowLinear", dtype),
-        )
 
     def forward(
         self, params: dict[str, np.ndarray], inputs: np.ndarray
     ) -> tuple[dict, np.ndarray]:
-        ctxs, inputs = super().forward(params, inputs)
-        tree_map(npdist.all_reduce, inputs, comm=self.comm.tp_comm)
-        return ctxs, inputs
+        ctxs, outputs = super().forward(params, inputs)
+        tree_map(npdist.all_reduce, outputs, comm=self.comm.tp_comm)
+        return ctxs, outputs
 
     def backward(
         self,
--- tests/test_tensor_parallel.py
+++ tests/test_tensor_parallel.py
@@ -48,24 +48,20 @@ def test_mlp(
     params = mlp.init_params(rng=rng)
     params_tp = mlp_tp.init_params(rng=rng)
 
-    npdist.scatter(
-        params["Linear1"]["weight"],
-        params_tp["ColumnLinear"]["weight"],
-        axis=1,
-        comm=comm.tp_comm,
-    )
-    npdist.scatter(
-        params["Linear1"]["bias"],
-        params_tp["ColumnLinear"]["bias"],
-        axis=0,
-        comm=comm.tp_comm,
-    )
-    npdist.scatter(
-        params["Linear2"]["weight"],
-        params_tp["RowLinear"]["weight"],
-        axis=0,
-        comm=comm.tp_comm,
-    )
+    for i, weight in enumerate(["Linear1", "Linear2"]):
+        npdist.scatter(
+            params[weight]["weight"],
+            params_tp[weight]["weight"],
+            axis=1 if i == 0 else 0,
+            comm=comm.tp_comm,
+        )
+        if i == 0:
+            npdist.scatter(
+                params[weight]["bias"],
+                params_tp[weight]["bias"],
+                axis=0,
+                comm=comm.tp_comm,
+            )
 
     ctx, out = mlp(params, inputs)
     ctx_tp, out_tp = mlp_tp(params_tp, inputs)
@@ -73,18 +69,17 @@ def test_mlp(
     grads, d_out = mlp.backward(ctx, np.ones_like(out))
     grads_tp, d_out_tp = mlp_tp.backward(ctx_tp, np.ones_like(out_tp))
 
-    clw = np.zeros_like(grads["Linear1"]["weight"])
-    clb = np.zeros_like(grads["Linear1"]["bias"])
-    rlw = np.zeros_like(grads["Linear2"]["weight"])
-
-    npdist.all_gather(
-        grads_tp["ColumnLinear"]["weight"], clw, axis=-1, comm=comm.tp_comm
-    )
-    npdist.all_gather(grads_tp["ColumnLinear"]["bias"], clb, axis=0, comm=comm.tp_comm)
-    npdist.all_gather(grads_tp["RowLinear"]["weight"], rlw, axis=0, comm=comm.tp_comm)
-
-    np.testing.assert_allclose(d_out, d_out_tp, rtol=1e-5, atol=1e-5)
     np.testing.assert_allclose(out, out_tp, atol=1e-5)
-    np.testing.assert_allclose(grads["Linear1"]["weight"], clw, atol=1e-5)
-    np.testing.assert_allclose(grads["Linear1"]["bias"], clb, atol=1e-5)
-    np.testing.assert_allclose(grads["Linear2"]["weight"], rlw, atol=1e-5)
+    np.testing.assert_allclose(d_out, d_out_tp, rtol=1e-5, atol=1e-5)
+
+    for i, weight in enumerate(["Linear1", "Linear2"]):
+        w = np.zeros_like(grads[weight]["weight"])
+        npdist.all_gather(
+            grads_tp[weight]["weight"], w, axis=-1 if i == 0 else 0, comm=comm.tp_comm
+        )
+        np.testing.assert_allclose(grads[weight]["weight"], w, atol=1e-5)
+
+        if i == 0:
+            b = np.zeros_like(grads[weight]["bias"])
+            npdist.all_gather(grads_tp[weight]["bias"], b, axis=0, comm=comm.tp_comm)
+            np.testing.assert_allclose(grads[weight]["bias"], b, atol=1e-5)
